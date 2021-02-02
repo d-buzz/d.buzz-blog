70607e5fd953f5494bb4edf8e1e88ba990f67580
Jhune Carlo Trogelio <jhunecarlo.trogelio@gmail.com>
2021-02-03 06:38:54 +0800

    added methods on auth saga

--- src/store/auth/sagas.js
+++ src/store/auth/sagas.js
@@ -1,16 +1,25 @@
-import { call, put, takeEvery, select } from 'redux-saga/effects'
+import { call, put, takeEvery } from 'redux-saga/effects'
 import {
   AUTHENTICATE_USER_REQUEST,
   authenticateUserSuccess,
   authenticateUserFailure,
+
+  setMuteList,
+
+  setAccountList,
+
 } from './actions'
 
 import {
   keychainSignIn,
   fetchProfile,
+  isWifValid,
+  packLoginData,
+  getCommunityRole,
+  fetchMuteList,
 } from 'services/api'
 
-import { generateSession, readSession } from 'services/helper'
+import { generateSession } from 'services/helper'
 
 function* authenticateUserRequest(payload, meta) {
   const { password, useKeychain } = payload
@@ -28,7 +37,7 @@ function* authenticateUserRequest(payload, meta) {
     users = JSON.parse(users)
   }
 
-  // const initialUsersLength = users.length
+  const initialUsersLength = users.length
 
   if (!accounts) {
     accounts = []
@@ -47,14 +56,56 @@ function* authenticateUserRequest(payload, meta) {
         if (profile) {
           profile = profile[0]
         }
-
+        
+        if (profile) {
+          const pubWif = profile['posting'].key_auths[0][0]
+          try {
+            const isValid = isWifValid(password, pubWif)
+            user.isAuthenticated = isValid
+            user.loginData = packLoginData(username, password)
+          } catch (e) {
+            user.isAuthenticated = false
+          }
+        } else {
+          user.isAuthenticated = false
+        }
       }
+
+      if (user.isAuthenticated) {
+        const isSubscribe = yield call(getCommunityRole, username)
+        user.isSubscribe = isSubscribe
+        user.active = true
+
+        let mutelist = yield call(fetchMuteList, username)
+
+        mutelist = [ ...new Set(mutelist.map(item => item.following))]
+
+        yield put(setMuteList(mutelist))
+
+        const session = generateSession(user)
+
+        const isInAccountList = accounts.filter(item => item.username === username)
+
+        if (isInAccountList.length === 0) {
+          users.push(session)
+          accounts.push({ username, keychain: useKeychain })
+        }
+
+        yield call([localStorage, localStorage.clear])
+        yield call([localStorage, localStorage.setItem], 'user', JSON.stringify(users))
+        yield call([localStorage, localStorage.setItem], 'active', username)
+        yield call([localStorage, localStorage.setItem], 'accounts', JSON.stringify(accounts))
+        yield put(setAccountList(accounts))
+      }
+
+      if (initialUsersLength > 0 && users.length !== initialUsersLength) {
+        window.location.reload()
+      }
+      yield put(authenticateUserSuccess(user, meta))
     }
-  } catch(e) {
-    console.log(e)
+  } catch(error) {
+    yield put(authenticateUserFailure(error, meta))
   }
-
-
 }
 
 
