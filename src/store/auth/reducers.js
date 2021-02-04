6dc45fb28bb51ce1a7ab4a0b162e0d4ac201f33d
Jhune Carlo Trogelio <jhunecarlo.trogelio@gmail.com>
2021-02-04 12:51:38 +0800

    initialized the getting of saved users on reducers

--- src/store/auth/reducers.js
+++ src/store/auth/reducers.js
@@ -1,16 +1,27 @@
 import {
   AUTHENTICATE_USER_SUCCESS,
+  GET_SAVED_USERS_SUCCESS,
+  SET_OPACITY_USERS,
+  SET_HAS_AGREED_PAYOUT,
 } from './actions'
 import { fromJS } from 'immutable'
 
 const defaultState = fromJS({
   user: {},
+  opacityUsers: [],
+  payoutAgreed: false,
 })
 
 export const auth = (state = defaultState, { type, payload }) => {
   switch (type) {
   case AUTHENTICATE_USER_SUCCESS:
     return state.set('user', payload)
+  case GET_SAVED_USERS_SUCCESS:
+    return state.set('user', payload)
+  case SET_OPACITY_USERS:
+    return state.set('opacityUsers', payload)
+  case SET_HAS_AGREED_PAYOUT:
+    return state.set('payoutAgreed', payload)
   default: 
     return state
   }
