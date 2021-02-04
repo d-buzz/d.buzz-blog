d3b854562199e5fed7d539b72996ea1acb1d2d7b
Jhune Carlo Trogelio <jhunecarlo.trogelio@gmail.com>
2021-02-04 15:10:28 +0800

    added get saved theme on reducers

--- src/store/settings/reducers.js
+++ src/store/settings/reducers.js
@@ -1,10 +1,14 @@
 import {
   SET_RPC_NODE,
+  GET_SAVED_THEME_SUCCESS,
+  GENERATE_STYLES,
 } from './actions'
 
 import { fromJS } from 'immutable'
 
 const defaultState = fromJS({
+  theme: {},
+  themeStyles: {},
   rpcNode: 'https://api.hive.blog',
 })
 
@@ -12,6 +16,10 @@ export const settings = (state = defaultState, { type, payload }) => {
   switch (type) {
   case SET_RPC_NODE:
     return state.set('rpcNode', payload)
+  case GET_SAVED_THEME_SUCCESS:
+    return state.set('theme', payload)
+  case GENERATE_STYLES:
+    return state.set('themeStyles', payload)
   default:
     return state
   }
