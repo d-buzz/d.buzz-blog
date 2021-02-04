c013a6bfc8a2405db26dfd253d091d10b1f93898
Jhune Carlo Trogelio <jhunecarlo.trogelio@gmail.com>
2021-02-04 15:09:44 +0800

    added get saved theme in actions

--- src/store/settings/actions.js
+++ src/store/settings/actions.js
@@ -29,3 +29,33 @@ export const checkVersionSuccess = (response, meta) => ({
   payload: response,
   meta,
 })
+
+export const GET_SAVED_THEME_REQUEST = 'GET_SAVED_THEME_REQUEST'
+export const GET_SAVED_THEME_SUCCESS = 'GET_SAVED_THEME_SUCCESS'
+export const GET_SAVED_THEME_FAILURE = 'GET_SAVED_THEME_FAILURE'
+
+export const getSavedThemeRequest = () => ({
+  type: GET_SAVED_THEME_REQUEST,
+  meta: {
+    thunk: true,
+  },
+})
+
+export const getSavedThemeSuccess = (response, meta) => ({
+  type: GET_SAVED_THEME_SUCCESS,
+  payload: response,
+  meta,
+})
+
+export const getSavedThemeFailure = (error, meta) => ({
+  type: GET_SAVED_THEME_FAILURE,
+  payload: error,
+  meta,
+})
+
+export const GENERATE_STYLES = 'GENERATE_STYLES'
+
+export const generateStyles = (theme) => ({
+  type: GENERATE_STYLES,
+  payload: theme,
+})
