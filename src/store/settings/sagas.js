02dcac8d8587d15bf27c5bc2d91469b1b559c8fe
Jhune Carlo Trogelio <jhunecarlo.trogelio@gmail.com>
2021-02-11 15:10:13 +0800

    added set theme in sagas

--- src/store/settings/sagas.js
+++ src/store/settings/sagas.js
@@ -6,9 +6,15 @@ import {
 
   CHECK_VERSION_REQUEST,
   checkVersionSuccess,
+
   GET_SAVED_THEME_REQUEST,
   getSavedThemeFailure,
   getSavedThemeSuccess,
+  
+  SET_THEME_REQUEST,
+  setThemeSuccess,
+  setThemeFailure,
+
 } from './actions'
 
 import {
@@ -47,7 +53,7 @@ function* checkVersionRequest(meta) {
 }
 
 function* getSavedThemeRequest(meta) {
-  let theme = { mode: 'light'}
+  let theme = { mode: 'light' }
   try {
     let saved = yield call([localStorage, localStorage.getItem], 'theme')
     saved = JSON.parse(saved)
@@ -60,8 +66,24 @@ function* getSavedThemeRequest(meta) {
   }
 }
 
+function* setThemeRequest(payload, meta) {
+  try {
+    const { mode } = payload
+    const theme = { mode }
+    yield call([localStorage, localStorage.setItem], 'theme', JSON.stringify(theme))
+    yield put(setThemeSuccess(theme, meta))
+  } catch(error) {
+    yield put(setThemeFailure(error, meta))
+  }
+}
+
+
 function* watchGetBestRpcNode({ meta }) {
   yield call(getBestRPCNode, meta)
+}
+
+function* watchSetThemeRequest({ payload, meta }) {
+  yield call(setThemeRequest, payload ,meta)
 }
 
 function* watchCheckVersionRequest({ meta }) {
@@ -74,6 +96,7 @@ function* watchGetSavedThemeRequest({ meta }) {
 
 export default function* sagas() {
   yield takeEvery(GET_BEST_RPC_NODE, watchGetBestRpcNode)
+  yield takeEvery(SET_THEME_REQUEST, watchSetThemeRequest)
   yield takeEvery(CHECK_VERSION_REQUEST, watchCheckVersionRequest)
   yield takeEvery(GET_SAVED_THEME_REQUEST, watchGetSavedThemeRequest)
 }
