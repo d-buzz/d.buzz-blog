3ebd087157b0160ecd0b91b722ff38383b058112
Jhune Carlo Trogelio <jhunecarlo.trogelio@gmail.com>
2021-02-11 15:19:45 +0800

    added store methods on the registry of redux store

--- src/store/index.js
+++ src/store/index.js
@@ -2,16 +2,28 @@ import { combineReducers } from 'redux'
 import { fork, all } from 'redux-saga/effects'
 
 import { tests } from './tests/reducers'
+import { auth } from './auth/reducers'
+import { settings } from './settings/reducers'
+import { posts } from './posts/reducers'
 import { reducer as thunkReducer } from 'redux-saga-thunk'
 import * as testSagas from './tests/sagas'
+import * as authSagas from './auth/sagas'
+import * as settingsSagas from './settings/sagas'
+import * as postSagas from './posts/sagas'
 
 export const rootReducer = combineReducers({
   thunk: thunkReducer,
   tests,
+  auth,
+  settings,
+  posts,
 })
   
 export function* rootSaga() {
   yield all([
+    ...Object.values(authSagas),
     ...Object.values(testSagas),
+    ...Object.values(settingsSagas),
+    ...Object.values(postSagas),
   ].map(fork))
 }
