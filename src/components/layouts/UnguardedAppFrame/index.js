1d236dc56875033d811236ef8ba579fc9fb8d259
Jhune Carlo Trogelio <jhunecarlo.trogelio@gmail.com>
2021-02-12 03:32:16 +0800

    initialized the unguarded appframe

---
+++ src/components/layouts/UnguardedAppFrame/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const UnguardedAppFrame = () => {
+  console.log('unguarded loaded')
+  return (
+    <React.Fragment>
+      
+    </React.Fragment>
+  )
+}
+
+export default UnguardedAppFrame
