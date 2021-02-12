30003e4fcbbe145e7d96cfc1baab14ddf5e13c58
Jhune Carlo Trogelio <jhunecarlo.trogelio@gmail.com>
2021-02-12 14:57:39 +0800

    removed the test redux

--- src/store/index.js
+++ src/store/index.js
@@ -1,19 +1,16 @@
 import { combineReducers } from 'redux'
 import { fork, all } from 'redux-saga/effects'
 
-import { tests } from './tests/reducers'
 import { auth } from './auth/reducers'
 import { settings } from './settings/reducers'
 import { posts } from './posts/reducers'
 import { reducer as thunkReducer } from 'redux-saga-thunk'
-import * as testSagas from './tests/sagas'
 import * as authSagas from './auth/sagas'
 import * as settingsSagas from './settings/sagas'
 import * as postSagas from './posts/sagas'
 
 export const rootReducer = combineReducers({
   thunk: thunkReducer,
-  tests,
   auth,
   settings,
   posts,
@@ -22,7 +19,6 @@ export const rootReducer = combineReducers({
 export function* rootSaga() {
   yield all([
     ...Object.values(authSagas),
-    ...Object.values(testSagas),
     ...Object.values(settingsSagas),
     ...Object.values(postSagas),
   ].map(fork))
