83c0c3e8f7fe597ae851a32a1b84f43016648e54
Jhune Carlo Trogelio <jhunecarlo.trogelio@gmail.com>
2021-02-21 00:17:47 +0800

    Fixed LoginModal BUG (#5)
    * fixed login modal bug on appBar
    
    * removed loginModal from appframe and move it to appbar
    
    * added buzzers word

--- src/components/layouts/AppFrame/index.js
+++ src/components/layouts/AppFrame/index.js
@@ -1,13 +1,11 @@
-import React, { useState, useEffect } from 'react'
-import { AppBar, LoginModal, GuardedAppFrame } from 'components'
+import React from 'react'
+import { AppBar, GuardedAppFrame, UnguardedAppFrame } from 'components'
 import { createUseStyles } from 'react-jss'
 import { useLocation } from 'react-router-dom'
-import queryString from 'query-string'
 import { isMobile } from 'react-device-detect'
 import { Container } from 'react-bootstrap'
 import { StickyContainer } from 'react-sticky'
 import { connect } from 'react-redux'
-import UnguardedAppFrame from '../UnguardedAppFrame'
 
 const useStyles = createUseStyles({
   main: {
@@ -18,19 +16,10 @@ const useStyles = createUseStyles({
     margin: '0 auto',
   },
   guardedContainer: {
-    width: 'max-content',
-    '@media (min-width: 1250px)': {
+    '@media (min-width: 1100px)': {
       margin: '0 auto !important',
       '&.container': {
-        margin: '0 auto',
-        maxWidth: 'max-content',
-      },
-    },
-    '@media (max-width: 768px)': {
-      margin: '0 auto !important',
-      '&.container': {
-        margin: '0 auto',
-        minWidth: 'max-content',
+        maxWidth: '900px',
       },
     },
   },
@@ -47,49 +36,22 @@ const useStyles = createUseStyles({
 const AppFrame = (props) => {
   const classes = useStyles()
   const { user, route } = props
-  const { pathname, search } = useLocation()
-  let { isAuthenticated } = user
-  isAuthenticated = false
-  const params = queryString.parse(search) || ''
-  const referrer = document.referrer
+  const { pathname } = useLocation()
+  const { isAuthenticated } = user
+
   
   let containerClass = classes.guardedContainer
   const unGuardedRoute = (pathname.match(/^\/login/) || !isAuthenticated)
-  
-  const [showLogin, setShowLogin] = useState(false)
-  const [signUpConfirmation, setSignUpConfirmation] = useState(false)
 
   if (unGuardedRoute) {
     containerClass = classes.unGuardedContainer
   }
 
-  const checkIfLogin = () => {
-    if (!isAuthenticated) {
-      setShowLogin(true)
-    } else {
-      setShowLogin(false)
-    }
-  }
-
-  const handleClickCloseLoginModal = () => {
-    setShowLogin(false)
-  }
-
-  useEffect(() => {
-    if ((params.status === 'success') && referrer === 'https://hiveonboard.com/') {
-      setSignUpConfirmation(true)
-      checkIfLogin()
-    } else {
-      setShowLogin(false)
-    }
-    // eslint-disable-next-line
-  }, [params, pathname]);
-
   return (
     <React.Fragment>
-      {!isAuthenticated && (<AppBar />)}
       {!isMobile && (
         <Container className={containerClass}>
+          <AppBar />
           <StickyContainer>
             {isAuthenticated && (
               <GuardedAppFrame pathname={pathname} route={route} />
@@ -100,10 +62,6 @@ const AppFrame = (props) => {
           </StickyContainer>
         </Container>
       )}
-      <LoginModal
-        show={showLogin}
-        onHide={handleClickCloseLoginModal}
-        signUpConfirmation={signUpConfirmation}/>
     </React.Fragment>
   )
 }
