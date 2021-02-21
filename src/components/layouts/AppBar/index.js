e4085a62dfc04617b1ffef3cdb3a9792f2a65f6a
Jhune Carlo Trogelio <jhunecarlo.trogelio@gmail.com>
2021-02-21 17:26:12 +0800

    fixed the appbar

--- src/components/layouts/AppBar/index.js
+++ src/components/layouts/AppBar/index.js
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from 'react'
 import Container from 'react-bootstrap/Container'
 import Navbar from 'react-bootstrap/Navbar'
-import Nav from 'react-bootstrap/Nav'
-import { BrandIcon, 
+import classNames from 'classnames'
+import { 
+  Avatar,
+  BrandIcon, 
   BrandDarkIcon, 
   BackArrowIcon,  
 } from 'components/elements'
@@ -14,17 +16,24 @@ import { isMobile } from 'react-device-detect'
 import { connect } from 'react-redux'
 import { 
   Button, 
-  Hidden, 
-  Avatar,
-  Typography,
-  Fab,
+  Hidden,
+  IconButton,
 } from '@material-ui/core'
-import { IconButton } from 'components/elements'
+import HomeIcon from '@material-ui/icons/Home'
+import TrendingUpIcon from '@material-ui/icons/TrendingUp'
+import UpdateIcon from '@material-ui/icons/Update'
+import SettingsIcon from '@material-ui/icons/Settings'
+import SupervisorAccountIcon from '@material-ui/icons/SupervisorAccount'
+import {
+  Menu,
+  MenuList,
+  MenuButton,
+  MenuLink,
+} from '@reach/menu-button'
 import AddIcon from '@material-ui/icons/Add'
 import ExitToAppIcon from '@material-ui/icons/ExitToApp'
 import { bindActionCreators } from 'redux'
 import NotificationsNoneIcon from '@material-ui/icons/NotificationsNone'
-import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown'
 import { signoutUserRequest } from 'store/auth/actions'
 import { useLastLocation } from 'react-router-last-location'
 import { pending } from 'redux-saga-thunk'
@@ -54,6 +63,72 @@ const useStyles = createUseStyles(theme => ({
   leftAdjust: {
     marginLeft: 7,
   },
+  profileButton: {
+    height: 'max-content',
+    borderRadius: 50,
+    cursor: 'pointer',
+    transitionDuration: '0.3s',
+    transitionProperty: 'background-color',
+    ...theme.nav.profile.wrapper,
+  },
+  profileUsername: {
+    paddingLeft: 10,
+    paddingRight: 10,
+    marginTop: 10,
+  },
+  avatarWrapper: {
+    minHeight: 35,
+    paddingLeft: 5,
+    paddingRight: 5,
+    paddingTop: 5,
+  },
+  buzzButton: {
+    borderRadius: 35,
+    margin: 'auto',
+    transitionDuration: '0.3s',
+    transitionProperty: 'background-color',
+    ...theme.nav.buzz.wrapper,
+  },
+  buzzWrapper: {
+    height: 45,
+    width: 45,
+    padding: 10,
+    alignItems: 'center',
+  },
+  notificationButton: {
+    borderRadius: 35,
+    margin: 'auto',
+    transitionDuration: '0.3s',
+    transitionProperty: 'background-color',
+    ...theme.nav.notification.wrapper,
+  },
+  notificationWrapper: {
+    height: 45,
+    width: 45,
+    padding: 10,
+    alignItems: 'center',
+  },
+  menulistWrapper: {
+    display: 'block',
+    whiteSpace: 'nowrap',
+    borderRadius: 5,
+    border: 'solid 1px hsla(0, 0%, 0%, 0.25)',
+    background: 'hsla(0, 100%, 100%, 0.99)',
+    outline: 'none',
+    padding: '1rem 0',
+    fontSize: '85%',
+    cursor: 'pointer',
+    '& a': {
+      display: 'block',
+      color: 'inherit',
+      font: 'inherit',
+      textDecoration: 'initial',
+      padding: '5px 20px',
+      '&:hover': {
+        backgroundColor: '#f5f8fa',
+      },
+    },
+  },
 }))
 
 const AppBar = (props) => {
@@ -67,6 +142,7 @@ const AppBar = (props) => {
   const { pathname } = location
   const { username } = user
 
+  const minify = false // set value for testing...
   const [open, setOpen] = useState(false)
 
   const handleClickBackButton = () => {
@@ -93,6 +169,18 @@ const AppBar = (props) => {
     signoutUserRequest()
   }
   
+  const handleClickBuzz = () => {
+    alert('buzz me')
+  }
+
+  const handleClickNotification = () => {
+    alert('notification')
+  }
+
+  const handleClickProfile = () => {
+    alert('profile')
+  }
+
   let title = 'Home'
 
   if(pathname.match(/(\/c\/)/)) {
@@ -151,40 +239,59 @@ const AppBar = (props) => {
             <React.Fragment>
               <Hidden only="xs">
                 <SearchField className={classes.search} disableTips={true} />
-              </Hidden>
-              <Nav style={{ marginLeft: 60 }}>
-                {isAuthenticated && (
-                  <React.Fragment>
-                    {/* <div onClick={onClick} >
-                    <Link to={path}>
-                      <IconWrapper style={{ textAlign: 'right' }} className={iconClass}>{icon}</IconWrapper>
-                      {username}
-                    </Link>
-                    </div> */}
-                    
-                    <Avatar className={classes.leftAdjust} src={`https://images.hive.blog/u/${username}/avatar/small`} />
-                    &nbsp;&nbsp;&nbsp;
-                    <Typography variant="subtitle1" style={{ paddingTop: 5 }} className={classes.leftAdjust}>{username}</Typography>
-                    &nbsp;&nbsp;&nbsp;
-                    &nbsp;&nbsp;&nbsp;
-                    <Fab color="secondary" size="small" aria-label="add" className={classes.leftAdjust} disableElevation>
-                      <AddIcon />
-                    </Fab>
-                    &nbsp;
-                    <Fab color="secondary" size="small" aria-label="add" className={classes.leftAdjust} disableElevation>
-                      <NotificationsNoneIcon />
-                    </Fab>
-                    &nbsp;
-                    <Fab color="secondary" size="small" aria-label="add" className={classes.leftAdjust} disableElevation>
-                      <KeyboardArrowDownIcon />
-                    </Fab>
-                    &nbsp;
-                    <Fab onClick={handleClickLogout} color="secondary" size="small" aria-label="add" className={classes.leftAdjust} disableElevation>
-                      <ExitToAppIcon />
-                    </Fab>
-                  </React.Fragment>
-                )}
-              </Nav>
+              </Hidden>            
+              {isAuthenticated && (
+                <React.Fragment>
+                  {!minify && (
+                    <React.Fragment>
+                      <div style={{ display: 'inline-flex' }}>
+                        <div className={classes.buzzButton}>
+                          <div className={classes.buzzWrapper} onClick={handleClickBuzz}>
+                            <AddIcon />
+                          </div>
+                        </div>
+                        &nbsp;
+                        <div className={classes.notificationButton}>
+                          <div className={classes.notificationWrapper} onClick={handleClickNotification}>
+                            <NotificationsNoneIcon />
+                          </div>
+                        </div>
+                        &nbsp;
+                        <Menu>
+                          <MenuButton
+                            className={classNames(classes.profileButton, classes.avatarWrapper)}
+                            style={{ display: 'table-cell', width: 'auto', height: '100%', border: 'none' }}
+                          >
+                            <div style={{ display: 'inline-flex', top: '50%', bottom: '50%' }}>
+                              <Avatar author={username} />
+                              <h6 className={classes.profileUsername}>@{username}</h6>
+                            </div>
+                          </MenuButton>
+                          <MenuList style={{ width: 'auto' }} className={classes.menulistWrapper}>
+                            <MenuLink onSelect={handleClickProfile} style={{ padding: 'auto', '&: hover':{ backgroundColor: 'red' } }}>
+                              <div>
+                                <Avatar height={40} author={username} style={{ marginBottom: -10 }} />
+                                <strong style={{ paddingLeft: 30, marginBottom: 0, fontSize: 15 }}>Profile</strong>
+                                <div style={{ marginTop: -15, paddingLeft: 50, paddingBottom: 5 }}>
+                                  <span style={{ fontSize: 13 }}>See your Profile</span>
+                                </div>
+                              </div>
+                            </MenuLink>
+                            <MenuLink to="/latest"><HomeIcon /><label style={{ paddingLeft: 15, marginBottom: 0, fontSize: 15 }}>Home</label></MenuLink>
+                            <MenuLink to="/latest"><TrendingUpIcon /><label style={{ paddingLeft: 15, marginBottom: 0, fontSize: 15 }}>Trending</label></MenuLink>
+                            <MenuLink to="/latest"><UpdateIcon /><label style={{ paddingLeft: 15, marginBottom: 0, fontSize: 15 }}>Latest</label></MenuLink>
+                            <MenuLink to="/latest"><SettingsIcon /><label style={{ paddingLeft: 15, marginBottom: 0, fontSize: 15 }}>User Settings</label></MenuLink>
+                            <MenuLink to="/latest"><SupervisorAccountIcon /><label style={{ paddingLeft: 15, marginBottom: 0, fontSize: 15 }}>Switch Account</label></MenuLink>
+                            <MenuLink onSelect={handleClickLogout}> <ExitToAppIcon /><label style={{ paddingLeft: 15, marginBottom: 0, fontSize: 15 }}>Signout</label></MenuLink>
+                          </MenuList>
+                        </Menu>
+                      </div>
+                      
+                    </React.Fragment>
+                  )}
+                </React.Fragment>
+              )}
+              
             </React.Fragment>
           )}
           {!isAuthenticated && (
