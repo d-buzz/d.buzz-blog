1adb64488a8280c38e7a5963fcadfa65e3eb5df4
Jhune Carlo Trogelio <jhunecarlo.trogelio@gmail.com>
2021-03-18 07:04:10 +0800

    refactored agreed payout variable

--- src/store/auth/reducers.js
+++ src/store/auth/reducers.js
@@ -2,7 +2,7 @@ import {
   AUTHENTICATE_USER_SUCCESS,
   GET_SAVED_USER_SUCCESS,
   SET_OPACITY_USERS,
-  SET_HAS_AGREED_PAYOUT,
+  SET_HAS_PAYOUT_AGREED,
   SET_FROM_LANDING,
   SET_MUTE_LIST,
   MUTE_USER_SUCCESS,
@@ -26,7 +26,7 @@ export const auth = (state = defaultState, { type, payload }) => {
     return state.set('user', payload)
   case SET_OPACITY_USERS:
     return state.set('opacityUsers', payload)
-  case SET_HAS_AGREED_PAYOUT:
+  case SET_HAS_PAYOUT_AGREED:
     return state.set('payoutAgreed', payload)
   case SET_FROM_LANDING:
     return state.set('fromLanding', payload)
