3fdd3ec6228438cb7c15fad572f676a1d9fc0c3d
Jhune Carlo Trogelio <jhunecarlo.trogelio@gmail.com>
2021-03-21 03:00:11 +0800

    added profile request on reducers saga

--- src/store/profile/reducers.js
+++ src/store/profile/reducers.js
@@ -1,4 +1,6 @@
 import {
+  GET_PROFILE_SUCCESS,
+  GET_ACCOUNT_POSTS_SUCCESS,
   CLEAR_ACCOUNT_POSTS,
   CLEAR_ACCOUNT_REPLIES,
 } from './actions'
@@ -6,12 +8,17 @@ import {
 import { fromJS } from 'immutable'
 
 const defaultState = fromJS({
+  profile: {},
   posts: [],
   replies: [],
 })
 
 export const profile = (state = defaultState, { type, payload }) => {
   switch (type) {
+  case GET_PROFILE_SUCCESS:
+    return state.set('profile', payload)
+  case GET_ACCOUNT_POSTS_SUCCESS:
+    return state.set('posts', payload)
   case CLEAR_ACCOUNT_POSTS:
     return state.set('posts', [])
   case CLEAR_ACCOUNT_REPLIES:
