1dc9c499d15da391da4b1618c31734b4b83d2e57
Jhune Carlo Trogelio <jhunecarlo.trogelio@gmail.com>
2021-03-21 09:30:41 +0800

    initialized the Account Post section

---
+++ src/components/sections/AccountPosts/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const AccountPosts = () => {
+  console.log('this is it')
+  return (
+    <React.Fragment>
+      <h1>Account post</h1>
+    </React.Fragment>
+  )
+}
+
+export default AccountPosts
