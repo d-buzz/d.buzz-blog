20e74cbeff3c6fba6406f9973f4d4f515616efb2
Jhune Carlo Trogelio <jhunecarlo.trogelio@gmail.com>
2021-03-21 15:24:25 +0800

    finalized the accoutn comment section on profile page (#22)

--- src/components/sections/AccountComments/index.js
+++ src/components/sections/AccountComments/index.js
@@ -1,11 +1,57 @@
-import React from 'react'
+import React, { useCallback } from 'react'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { pending } from 'redux-saga-thunk'
+import { InfiniteList } from 'components'
+import { getAccountCommentsRequest } from 'store/profile/actions'
 
-const AccountComments = () => {
+const AccountComments = (props) => {
+  const {
+    items = [],
+    loading,
+    getAccountCommentsRequest,
+    author,
+    last,
+    user,
+    mutelist,
+  } = props
+
+  const loadMorePosts =  useCallback(() => {
+    try {
+      const { permlink, author: start_author } = last
+      getAccountCommentsRequest(author, permlink, start_author)
+    } catch(e) { }
+    // eslint-disable-next-line
+  }, [last])
+
+  const muted = mutelist.includes(author)
+
   return (
     <React.Fragment>
-      <h1>Account JAN HOPE</h1>
+      {!muted && (
+        <React.Fragment>
+          <InfiniteList title={true} loading={loading} items={items} onScroll={loadMorePosts} unguardedLinks={!user.is_authenticated}/>
+          {(!loading && items.length === 0) &&
+          (<center><br/><h6>No comments from @{author}</h6></center>)}
+        </React.Fragment>
+      )}
+      {muted && (<center><br/><h6>This user is on your mutelist, unmute this user to view their comments</h6></center>)}
     </React.Fragment>
   )
 }
 
-export default AccountComments
+const mapStateToProps = (state) => ({
+  items: state.profile.get('comments'),
+  loading: pending(state, 'GET_ACCOUNT_COMMENTS_REQUEST'),
+  last: state.profile.get('lastComment'),
+  user: state.auth.get('user'),
+  mutelist: state.auth.get('mutelist'),
+})
+
+const mapDispatchToProps = (dispatch) => ({
+  ...bindActionCreators({
+    getAccountCommentsRequest,
+  }, dispatch),
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(AccountComments)
