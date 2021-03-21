779115662b183b420c1d9a31f60dc7a313c0d52d
Jhune Carlo Trogelio <jhunecarlo.trogelio@gmail.com>
2021-03-21 09:42:21 +0800

    initialized the Account Comments in sections

---
+++ src/components/sections/AccountComments/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AccountComments = () => {
+  return (
+    <React.Fragment>
+      <h1>Account JAN HOPE</h1>
+    </React.Fragment>
+  )
+}
+
+export default AccountComments
