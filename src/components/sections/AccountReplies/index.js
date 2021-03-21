15adb3d06d3be9754e5b4b8cc745c44e07ac8d1c
Jhune Carlo Trogelio <jhunecarlo.trogelio@gmail.com>
2021-03-21 10:22:07 +0800

    initialized the account replies && account comments (#20)
    * initialized the Account Comments in sections
    
    * intialized the Account Replies in sections

---
+++ src/components/sections/AccountReplies/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AccountReplies = () => {
+  return (
+    <React.Fragment>
+      <h1>Account Replies</h1>
+    </React.Fragment>
+  )
+}
+
+export default AccountReplies
