0832ff2a899f64764bb496931062ec4acb63b6a1
Jhune Carlo Trogelio <jhunecarlo.trogelio@gmail.com>
2021-03-21 16:42:52 +0800

    finalized the replies section on profile page

--- src/components/sections/AccountReplies/index.js
+++ src/components/sections/AccountReplies/index.js
@@ -1,11 +1,56 @@
-import React from 'react'
+import React, { useCallback } from 'react'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { pending } from 'redux-saga-thunk'
+import { InfiniteList } from 'components'
+import { getAccountRepliesRequest } from 'store/profile/actions'
 
-const AccountReplies = () => {
+const AccountReplies = (props) => {
+  const {
+    items = [],
+    loading,
+    last,
+    author,
+    getAccountRepliesRequest,
+    user,
+    mutelist,
+  } = props
+
+  const loadMorePosts =  useCallback(() => {
+    const { author: start_author, permlink } = last
+    getAccountRepliesRequest(author, permlink, start_author)
+    // eslint-disable-next-line
+  }, [last])
+
+  const muted = mutelist.includes(author)
+
   return (
     <React.Fragment>
-      <h1>Account Replies</h1>
+      {!muted && (
+        <React.Fragment>
+          <InfiniteList title={true} loading={loading} items={items} onScroll={loadMorePosts} unguardedLinks={!user.is_authenticated}/>
+          {(!loading && items.length === 0) &&
+          (<center><br/><h6>No replies found</h6></center>)}
+        </React.Fragment>
+      )}
+      {muted && (<center><br/><h6>This user is on your mutelist, unmute this user to view replies</h6></center>)}
     </React.Fragment>
   )
 }
 
-export default AccountReplies
+const mapStateToProps = (state) => ({
+  items: state.profile.get('replies'),
+  loading: pending(state, 'GET_ACCOUNT_REPLIES_REQUEST'),
+  last: state.profile.get('lastReply'),
+  user: state.auth.get('user'),
+  mutelist: state.auth.get('mutelist'),
+})
+
+const mapDispatchToProps = (dispatch) => ({
+  ...bindActionCreators({
+    getAccountRepliesRequest,
+  }, dispatch),
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(AccountReplies)
+
