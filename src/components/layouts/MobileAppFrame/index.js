9c26b19bf1792098dabc808fa8a3c59f027895aa
Jhune Carlo Trogelio <jhunecarlo.trogelio@gmail.com>
2021-04-12 22:45:29 +0800

    initialized  the mobile app frame (#74)
    * set the mobile appframe
    
    * initialized the mobile appframe

---
+++ src/components/layouts/MobileAppFrame/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MobileAppFrame = () => {
+  return (
+    <React.Fragment>
+      <h1>This is the Mobile Appframe</h1>
+    </React.Fragment>
+  )
+}
+
+export default MobileAppFrame
