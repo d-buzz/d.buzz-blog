08be74450a2746b8c85a96c5476496f971328df0
Jhune Carlo Trogelio <jhunecarlo.trogelio@gmail.com>
2021-05-23 22:50:09 +0800

    added reducers for setting the last blog on profile store

--- src/store/profile/reducers.js
+++ src/store/profile/reducers.js
@@ -5,15 +5,21 @@ import {
   GET_ACCOUNT_REPLIES_SUCCESS,
   CLEAR_ACCOUNT_POSTS,
   CLEAR_ACCOUNT_REPLIES,
+  SET_LAST_ACCOUNT_BLOG,
+  SET_LAST_ACCOUNT_REPLY,
+  SET_LAST_ACCOUNT_COMMENT,
 } from './actions'
 
 import { fromJS } from 'immutable'
 
 const defaultState = fromJS({
   profile: {},
-  posts: [],
+  blog: [],
   replies: [],
   comments: [],
+  last: [],
+  lastReply: [],
+  lastComment: [],
 })
 
 export const profile = (state = defaultState, { type, payload }) => {
@@ -21,15 +27,21 @@ export const profile = (state = defaultState, { type, payload }) => {
   case GET_PROFILE_SUCCESS:
     return state.set('profile', payload)
   case GET_ACCOUNT_BLOG_SUCCESS:
-    return state.set('posts', payload)
+    return state.set('blog', payload)
   case GET_ACCOUNT_COMMENTS_SUCCESS:
     return state.set('comments', payload)
   case GET_ACCOUNT_REPLIES_SUCCESS:
     return state.set('replies', payload)
   case CLEAR_ACCOUNT_POSTS:
-    return state.set('posts', [])
+    return state.set('blog', [])
   case CLEAR_ACCOUNT_REPLIES:
     return state.set('replies', [])
+  case SET_LAST_ACCOUNT_BLOG:
+    return state.set('last', payload)
+  case SET_LAST_ACCOUNT_REPLY:
+    return state.set('lastReply', payload)
+  case SET_LAST_ACCOUNT_COMMENT:
+    return state.set('lastComment', payload)  
   default:
     return state
   }
