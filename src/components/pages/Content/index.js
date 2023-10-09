fb15b845f7726d55fbb559308844ac33e782e038
Juneroy Quinimon <eroyjune@gmail.com>
2023-10-09 23:41:36 +0800

    feat: apply css boxShadow1

--- src/components/pages/Content/index.js
+++ src/components/pages/Content/index.js
@@ -408,7 +408,7 @@ const Content = (props) => {
     <React.Fragment>
       {!loadingContent && author && (
         <React.Fragment>
-          <div className={classNames(classes.visibilityVisible, classes.transformtranslateX414, classes.transition1)}>
+          <div className={classNames(classes.visibilityVisible, classes.transformtranslateX414, classes.transition1, classes.boxShadow1)}>
             reply section here
           </div>
           <div>
