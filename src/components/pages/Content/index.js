cf0c0eb8ee0b033f23d2af79b944d78782412e28
Juneroy Quinimon <eroyjune@gmail.com>
2023-10-11 17:18:17 +0800

    feat: append inner html data

--- src/components/pages/Content/index.js
+++ src/components/pages/Content/index.js
@@ -682,8 +682,8 @@ const Content = (props) => {
                             <pre className={classNames(classes.whiteSpacePreWrap)}>
                               <div className={classNames(classes.padding050, classes.displayBlock)}>
                                 <div className={classNames(classes.color242424,classes.fontSize14, classes.fontFamilySohe,classes.fontWeight400)}>
-                                  <p>
-                                    {reply.body}
+                                  <p dangerouslySetInnerHTML={{ __html: reply.body }} >
+                                    {/* {reply.body} */}
                                   </p>
                                 </div>
                               </div>
